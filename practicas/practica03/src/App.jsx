a06f21ba40a6408112201d46cc5ef903f21950c2
Sofia Goszko <sofiagoszko@gmail.com>
2025-09-21 19:42:33 -0300

    creacion del header

--- practicas/practica03/src/App.jsx
+++ practicas/practica03/src/App.jsx
@@ -3,12 +3,14 @@ import EquipoTalentoLab from './components/EquipoTalentoLab';
 import TarjetaProyecto from './components/TarjetaProyecto';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
+import Header from './components/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Header />
       <EquipoTalentoLab />
       <TarjetaProyecto />
       <Footer />
---
+++ practicas/practica03/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Container} from "react-bootstrap";
+
+const Header = () => {
+    return (
+        <header className="text-center">
+            <Container>
+                <h1 className="m-2 display-4 fw-bold">PRÁCTICA 3</h1>
+            </Container>
+        </header>
+    );
+
+};
+
+export default Header;
